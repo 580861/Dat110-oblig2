430a42e3481605bd4602d1a2b022c304e5aabba7
Hassan <aha403@gmail.com>
2021-02-19 22:25:28 +0100

    Task A done

--- src/no/hvl/dat110/messages/PublishMsg.java
+++ src/no/hvl/dat110/messages/PublishMsg.java
@@ -3,17 +3,45 @@ package no.hvl.dat110.messages;
 import no.hvl.dat110.common.TODO;
 
 public class PublishMsg extends Message {
-	
-	// message sent from client to create publish a message on a topic 
 
-	// TODO:
+	// message sent from client to create publish a message on a topic
+
+	// TODO: done
 	// Implement object variables - a topic and a message is required
 
 	// Constructor, get/set-methods, and toString method
 	// as described in the project text
-	
-	public String getMessage() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+
+	private String message, topic;
+
+	public PublishMsg(String user, String topic, String message) {
+		super(MessageType.PUBLISH, user);
+		this.topic = topic;
+		this.message = message;
 	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public PublishMsg(String msg) {
+		this.message = msg;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String msg) {
+		this.message = msg;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [type=\" + super.getType() + \", user=\" + super.getUser() + \", topic=\" + getTopic() + \"] ";
+	};
 }
--- src/no/hvl/dat110/messages/SubscribeMsg.java
+++ src/no/hvl/dat110/messages/SubscribeMsg.java
@@ -4,10 +4,34 @@ public class SubscribeMsg extends Message {
 
 	// message sent from client to subscribe on a topic 
 
-	// TODO:
+	// TODO: done
 	// Implement object variables - a topic is required
 
 	// Constructor, get/set-methods, and toString method
 	// as described in the project text
+	
+	private String topic;
+	
+	public SubscribeMsg(String user,String topic) {
+		super(MessageType.SUBSCRIBE,user);
+		this.topic=topic;
+		
+		
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "SubscribeMsg [topic=" + topic + "]";
+	}
+	
+	
 		
 }
